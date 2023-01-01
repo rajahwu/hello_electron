1e0d53bd2123b6d8b553b60fd0a52a408eb3b0d4
rajahwu <vmradford@gmail.com>
2022-12-31 23:17:14 -0500

    add preload script

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
 
 const createWindow = () => {
-    const window = new BrowserWindow({
+    const win = new BrowserWindow({
         width: 800,
         height: 600,
-    });
-
-    window.loadFile('index.html');
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+        }
+    })
+    ipcMain.handle('ping', () => 'ping')
+    win.loadFile('index.html');
 };
 
 app.whenReady().then(() => {
